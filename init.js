195afb6252d10cff3911a81e85c771cff949d97f
Joel Martin <joel@Joels-MacBook-Pro.local>
2024-10-03 14:21:27 +0100

    HandleMobileNav

--- init.js
+++ init.js
@@ -1,3 +1,23 @@
 (() => {
     console.log('Package running... | Martin Digital :)');
+
+    const navbar_menu = $('#navbar_menu');
+
+    navbar_menu.length > 0
+        ? handleMobileNav(navbar_menu)
+        : console.error('navbar-menu not found');
 })();
+
+function handleMobileNav(navbar_menu) {
+    let open = false;
+
+    navbar_menu.on('click', () => {
+        if (open) {
+            $('body').css('overflow', 'visible');
+        } else {
+            $('body').css('overflow', 'hidden');
+        }
+
+        open = !open;
+    });
+}
