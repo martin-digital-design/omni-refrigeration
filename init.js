ea9c13e6ca74f676d808870d2c0dc4a0588dc792
Joel Martin <joel@Joels-MacBook-Pro.local>
2024-10-04 15:06:16 +0100

    handleNavPosition

--- init.js
+++ init.js
@@ -1,11 +1,16 @@
 (() => {
     console.log('Package running... | Martin Digital :)');
 
-    const navbar_menu = $('#navbar_menu');
+    const navbar_menu = $('#navbar_menu'),
+        navbar = $('#navbar');
 
     navbar_menu.length > 0
         ? handleMobileNav(navbar_menu)
         : console.error('navbar-menu not found');
+
+    navbar.length > 0
+        ? handleNavPosition(navbar)
+        : console.error('navbar not found');
 })();
 
 function handleMobileNav(navbar_menu) {
@@ -20,4 +25,32 @@ function handleMobileNav(navbar_menu) {
 
         open = !open;
     });
+}
+
+function handleNavPosition(navbar) {
+    const navbar_top = navbar.offset().top;
+    const navbar_bottom = navbar_top + navbar.outerHeight();
+    let viewport_top = 0;
+    let ticking = false;
+
+    $(document).on('scroll', () => {
+        viewport_top = $(window).scrollTop();
+
+        if (!ticking) {
+            window.requestAnimationFrame(() => {
+                handleScroll(viewport_top);
+                ticking = false;
+            });
+
+            ticking = true;
+        }
+    });
+
+    function handleScroll(viewport_top) {
+        if (viewport_top > navbar_bottom) {
+            navbar.addClass('floating-nav');
+        } else {
+            navbar.removeClass('floating-nav');
+        }
+    }
 }
